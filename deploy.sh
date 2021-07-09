64dafa12c9dd3020a2dbcc221050a2741d79bac0
Matt Anderson <matt.anderson@mississauga.ca>
2021-07-09 08:30:06 -0400

    updated the deployment script

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:mattander/<REPO>.git main:gh-pages
+git push -f git@github.com:mattander/words.git main:gh-pages
 
 cd -
